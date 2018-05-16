162ba63cde001b10c248f374051a6b061ceb0b84
Alexander Lobur <alex.lobur@yellow.id>
2018-05-16 11:46:21 +0300

    Added view model di utils.

--- app/src/main/java/by/pavlovskii/ilya/mvvm/test/ui/activity/main/di/MainActivityModule.java
+++ app/src/main/java/by/pavlovskii/ilya/mvvm/test/ui/activity/main/di/MainActivityModule.java
@@ -2,16 +2,22 @@ package by.pavlovskii.ilya.mvvm.test.ui.activity.main.di;
 
 import android.support.annotation.NonNull;
 
+import javax.inject.Provider;
+
 import by.mvvmwrapper.dagger.map.ViewModelKey;
+import by.mvvmwrapper.dagger.qualifier.TopLevelDependency;
 import by.mvvmwrapper.dagger.scope.ActivityScope;
+import by.mvvmwrapper.dagger.utils.ViewModelDiUtils;
 import by.mvvmwrapper.viewmodel.BaseViewModel;
 import by.pavlovskii.ilya.mvvm.test.navigator.MainNavigator;
+import by.pavlovskii.ilya.mvvm.test.ui.activity.main.MainActivity;
 import by.pavlovskii.ilya.mvvm.test.ui.activity.main.MainViewModel;
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 import dagger.multibindings.IntoMap;
 import ru.terrakok.cicerone.Navigator;
+import timber.log.Timber;
 
 /**
  * Create with Android Studio<br>
@@ -35,4 +41,13 @@ public abstract class MainActivityModule {
     @ViewModelKey(MainViewModel.class)
     abstract BaseViewModel bindMainViewModel(@NonNull MainViewModel viewModel);
 
+
+    @Provides
+    @ActivityScope
+    @TopLevelDependency
+    static MainViewModel provideViewModel(@NonNull MainActivity activity,
+                                          @NonNull Provider<MainViewModel> viewModelProvider) {
+        Timber.d("provideViewModel");
+        return ViewModelDiUtils.provideViewModel(activity, MainViewModel.class, viewModelProvider);
+    }
 }
--- app/src/main/java/by/pavlovskii/ilya/mvvm/test/ui/fragments/timer/di/TimerViewModelModule.java
+++ app/src/main/java/by/pavlovskii/ilya/mvvm/test/ui/fragments/timer/di/TimerViewModelModule.java
@@ -1,22 +1,22 @@
 package by.pavlovskii.ilya.mvvm.test.ui.fragments.timer.di;
 
-import android.arch.lifecycle.ViewModelProvider;
-import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.NonNull;
+
+import javax.inject.Named;
+import javax.inject.Provider;
+
 import by.mvvmwrapper.dagger.map.ViewModelKey;
 import by.mvvmwrapper.dagger.qualifier.TopLevelDependency;
+import by.mvvmwrapper.dagger.scope.FragmentScope;
+import by.mvvmwrapper.dagger.utils.ViewModelDiUtils;
 import by.mvvmwrapper.viewmodel.BaseViewModel;
 import by.pavlovskii.ilya.mvvm.test.storage.Constants;
-import by.pavlovskii.ilya.mvvm.test.ui.activity.main.MainActivity;
-import by.pavlovskii.ilya.mvvm.test.ui.activity.main.MainViewModel;
 import by.pavlovskii.ilya.mvvm.test.ui.fragments.timer.TimerFragment;
 import by.pavlovskii.ilya.mvvm.test.ui.fragments.timer.TimerViewModel;
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 import dagger.multibindings.IntoMap;
-
-import javax.inject.Named;
 
 /**
  * Create with Android Studio<br>
@@ -43,12 +43,11 @@ public abstract class TimerViewModelModule {
     abstract BaseViewModel bindTimerViewModel(@NonNull TimerViewModel viewModel);
 
     @Provides
+    @FragmentScope
     @TopLevelDependency
-    static MainViewModel provideMainViewModel(@NonNull MainActivity activity,
-                                              @NonNull ViewModelProvider.Factory factory) {
-        return ViewModelProviders
-                .of(activity, factory)
-                .get(MainViewModel.class);
+    static TimerViewModel provideViewModel(@NonNull TimerFragment fragment,
+                                           @NonNull Provider<TimerViewModel> viewModelProvider) {
+        return ViewModelDiUtils.provideViewModel(fragment, TimerViewModel.class, viewModelProvider);
     }
 
 }
